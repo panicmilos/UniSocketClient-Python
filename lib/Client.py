4ae01f7a22455fd5d30a44fa325057420c384577
bjelicaluka <bjelicaluka99@gmail.com>
2020-04-07 19:11:20 +0200

    Defined Client interface.

---
+++ lib/Client.py
@@ -0,0 +1,19 @@
+class Client(object):
+    def __init__(self, name: str):
+        super().__init__(self)
+        self._name = name
+
+    def connect(self, host: str, port: int):
+        pass
+
+    def send_to_client(self, session_name: str, data):
+        pass
+
+    def send_to_clients(self, session_names: str, data):
+        pass
+
+    def broadcast(self, data):
+        pass
+
+    def disconnect(self):
+        pass
